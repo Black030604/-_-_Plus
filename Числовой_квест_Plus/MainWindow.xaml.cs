68fb5f8ce9dbc867ed6eb25ca2214eca5a890cb0
79393 <79393@DESKTOP-V4VQP93>
2025-02-08 15:25:00 +0300

    Добавьте файлы проекта.

---
+++ Числовой_квест_Plus/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Числовой_квест_Plus
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Random random = new Random();
+        private int playerHealth = 100;
+        private int potions = 3;
+        private int gold = 0;
+        private int arrows = 5;
+        private List<string> inventory = new List<string>();
+        private string[] dungeonMap = new string[10];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void sendBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
